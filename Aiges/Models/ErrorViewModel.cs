d97ff66eff3b1e9c001f6c0fa645c2a357b6e1ae
Gillvano <freegilioshouse@gmail.com>
2024-10-14 09:20:22 +0200

    Aiges project added

---
+++ Aiges/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Aiges.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
